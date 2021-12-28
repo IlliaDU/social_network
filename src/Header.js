022c498cf7f8e07ff44ba4ad4eb86c732e1a13e3
IlliaDU <dubin249@gmail.com>
2021-12-28 17:54:28 +0200

    commit 4

--- src/Header.js
+++ src/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
                 <li>1</li>
                 <li>2</li>
                 <li>3</li>
-                <li>4</li>
+                
             </ul>
         </div>
     )
