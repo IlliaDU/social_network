8dda08d44876320a67a8e1c7dfce06fd2b7345f7
IlliaDU <dubin249@gmail.com>
2021-12-28 17:28:29 +0200

    commit 1

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Technologies from './Technologies';
+import Header from './Header';
+
+const App = () => {
+  return (
+    <div>
+     <Technologies />
+     <Header />
+    </div>
+  );
+}
+
+export default App;
